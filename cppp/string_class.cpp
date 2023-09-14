aced9d6b77d16cbed51905e5bad814f7bf3318d7
gmltn205 <74577657+gmltn205@users.noreply.github.com>
2023-09-14 18:18:41 +0900

    vscode 설정 및 스트링클래스

---
+++ cppp/string_class.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string.h>
+
+class MyString
+{
+    char *string_content;
+    int string_length;
+
+public:
+    int length() const;
+    void print() const;
+    void println() const;
+    ~MyString();
+    MyString(char c);
+    MyString(char *str);
+    MyString(const MyString &str);
+};
+void MyString::println() const
+{
+    int i;
+    for (i = 0; i != string_length; i++)
+    {
+        std::cout << string_content[i];
+    }
+    std::cout << std::endl;
+}
+void MyString::print() const
+{
+    for (int i = 0; i != string_length; i++)
+    {
+        std::cout << string_content[i];
+    }
+}
+int MyString::length() const
+{
+    return string_length;
+}
+MyString::MyString(char c)
+{
+    string_content = new char[1];
+    string_content[0] = c;
+    string_length = 1;
+}
+MyString::MyString(char *str)
+{
+    string_length = strlen(str);
+    string_content = new char[string_length];
+    for (int i = 0; i != string_length; i++)
+        string_content[i] = str[i];
+}
+MyString::MyString(const MyString &str)
+{
+    string_length = str.string_length;
+    for (int i = 0; i != string_length; i++)
+    {
+        string_content[i] = str.string_content[i];
+    }
+}
+MyString::~MyString()
+{
+    delete[] string_content;
+}
+int main()
+{
+    MyString str1("hellow world!");
+    MyString str2("hey!!");
+
+    str1.print();
+    str2.println();
+}
