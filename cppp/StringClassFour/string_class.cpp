1a828f0d628264c8250b61ccb13ee129bc48af13
gmltn205 <74577657+gmltn205@users.noreply.github.com>
2023-09-15 15:36:29 +0900

    Update string_class.cpp

--- cppp/StringClassFour/string_class.cpp
+++ cppp/StringClassFour/string_class.cpp
@@ -5,16 +5,116 @@ class MyString
 {
     char *string_content;
     int string_length;
+    int memory_capacity;
 
 public:
     int length() const;
     void print() const;
     void println() const;
+    void reserve(int size);
+    int capacity();
     ~MyString();
+    MyString &insert(int loc, const MyString &str);
+    MyString &insert(int loc, const char *str);
+    MyString &insert(int loc, const char c);
+
+    MyString &assign(const MyString &str);
+    MyString &asssign(const char *str);
+
     MyString(char c);
     MyString(char *str);
     MyString(const MyString &str);
 };
+
+MyString &MyString::insert(int loc, const MyString &str)
+{
+    if (loc < 0 || loc > string_length)
+    {
+        return *this;
+    }
+
+    if (string_length + str.string_length > memory_capacity)
+    {
+        memory_capacity = string_length + str.string_length;
+        char *prev_string_content = string_content;
+        string_content = new char[memory_capacity];
+
+        int i;
+        for (i = 0; i < loc; i++)
+        {
+            string_content[i] = prev_string_content[i];
+        }
+        for (int j = loc; j < loc + str.length; i++)
+        {
+            string_content[j] = str.string_content[j];
+        }
+        for (; i < string_length; i++)
+        {
+            string_content[str.string_length + i] = prev_string_content[i];
+        }
+        delete[] prev_string_conent;
+        return *this;
+    }
+    delete[] string_content;
+    for (int i = 0; i != loc; i++)
+    {
+    }
+}
+void MyString::reserve(int size)
+{
+    if (size > memory_capacity)
+    {
+        char *prev_string_content = string_content;
+        string_content = new char[size];
+        memory_capacity = size;
+
+        for (int i = 0; i != string_length; i++)
+        {
+            string_content[i] = prev_string_content[i];
+        }
+
+        delete[] prev_string_content;
+    }
+}
+int MyString::capacity()
+{
+    return memory_capacity;
+}
+MyString &MyString::assign(const MyString &str)
+{
+    string_length = str.string_length;
+
+    if (memory_capacity < str.string_length)
+    {
+        delete[] string_content;
+        string_content = new char[string_length];
+        memory_capacity = str.string_length;
+    }
+
+    for (int i = 0; i != string_lengt; i++)
+    {
+
+        string_content[i] = str.string_content[i];
+    }
+    return *this;
+}
+MyString &MyString::assign(const char *str)
+{
+    string_length = strlen(str);
+    if (memory_capacity < string_length)
+    {
+        delete[] string_content;
+
+        string_content = new char[string_length];
+        memory_capacity = string_length;
+    }
+
+    for (int i = 0; i != string_length; i++)
+    {
+        string_content[i] = str[i];
+    }
+    return *this;
+}
 void MyString::println() const
 {
     int i;
